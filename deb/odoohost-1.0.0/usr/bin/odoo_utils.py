6a95939762f664fa0016f9a404a8af58103b6526
David Ramia <david.ramia@processcontrol.es>
2023-02-21 23:50:19 +0100

    ss

--- deb/odoohost-1.0.0/usr/bin/odoo_utils.py
+++ deb/odoohost-1.0.0/usr/bin/odoo_utils.py
@@ -1,9 +1,16 @@
+import glob
 import os
+
+
+def get_addons_path():
+    paths = ['/home/odoo/src/odoo/addons', '/home/odoo/src/enterprise', '/home/odoo/src/themes']
+    paths.extend([os.path.dirname(os.path.dirname(p)) for p in glob.glob('/home/odoo/src/user/**/__manifest__.py')])
+    return ','.join(set(paths))
 
 
 def get_odoo_args(args):
     odoo_args = [
-        '--addons-path', os.environ['ADDONS_PATH'],
+        '--addons-path', get_addons_path(),
         '--unaccent',
         '--no-database-list',
         '--proxy-mode',
@@ -11,7 +18,7 @@ def get_odoo_args(args):
         '--db_port=5432',
         '--db-filter=',
         '--data-dir', '/home/odoo/data',
-        '--config', '/home/odoo/.config/odoo/odoo.conf',
+        '--config', '/home/odoo/odoo.conf',
         '--workers=0',
     ]
     if not any(arg.startswith('--database') for arg in args):
