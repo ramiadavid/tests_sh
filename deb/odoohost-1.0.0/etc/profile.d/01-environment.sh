dcdec1c5ba6611fd67994948527f4129e53361ad
David Ramia <david.ramia@processcontrol.es>
2023-02-22 18:38:57 +0100

    ss

---
+++ deb/odoohost-1.0.0/etc/profile.d/01-environment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/root/bin"
+export PGPASSWORD="toBe72/*"
+export PGUSER=odoo
+export PGDATABASE=processcontrol
+export PGHOST=localhost
+export TZ=Europe/Madrid
+export LANG C.UTF-8
