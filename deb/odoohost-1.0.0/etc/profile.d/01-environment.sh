f90bb0bf466c625005d5730b2f8b9c27a8c87d2d
David Ramia <david.ramia@processcontrol.es>
2023-03-01 08:04:49 +0100

    ss

--- deb/odoohost-1.0.0/etc/profile.d/01-environment.sh
+++ deb/odoohost-1.0.0/etc/profile.d/01-environment.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/root/bin"
 export PGPASSWORD="toBe72/*"
 export PGUSER="odoo"
 export PGDATABASE="processcontrol"
 export PGHOST="localhost"
-export TZ="Europe/Madrid"
-export LANG="C.UTF-8"
-export ADDONS_PATH="/home/odoo/src/odoo/addons,/home/odoo/src/enterprise,/home/odoo/src/themes,$(find /home/odoo/src/user -name "__manifest__.py" -printf "%h\n" | xargs -I {} dirname {} | sort | uniq | paste -sd ",")"
+export ADDONS_PATH="/home/odoo/src/odoo/addons,/home/odoo/src/enterprise,/home/odoo/src/themes"
+
+custom_addons="$(find /home/odoo/src/user -name "__manifest__.py" -printf "%h\n" | xargs -I {} dirname {} | sort | uniq | paste -sd ",")"
+
+if [ -n "$var" ]; then
+  export ADDONS_PATH="${ADDONS_PATH},${custom_addons}"
+fi
 
