5b52299c48e6e25b0783b6a1fa8d8f39e5e89f9b
David Ramia <david.ramia@processcontrol.es>
2023-03-01 08:06:03 +0100

    ss

--- deb/odoohost-1.0.0/etc/profile.d/01-environment.sh
+++ deb/odoohost-1.0.0/etc/profile.d/01-environment.sh
@@ -7,7 +7,7 @@ export ADDONS_PATH="/home/odoo/src/odoo/addons,/home/odoo/src/enterprise,/home/o
 
 custom_addons="$(find /home/odoo/src/user -name "__manifest__.py" -printf "%h\n" | xargs -I {} dirname {} | sort | uniq | paste -sd ",")"
 
-if [ -n "$var" ]; then
+if [ -n "$custom_addons" ]; then
   export ADDONS_PATH="${ADDONS_PATH},${custom_addons}"
 fi
 
